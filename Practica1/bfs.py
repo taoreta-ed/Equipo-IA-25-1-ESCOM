4e1eaaceb8874311c44550b79f0979116bf8188a
taoreta <nora.castroeduardo@gmail.com>
2024-10-13 19:33:38 -0600

    Se agrega un nuevo grafo

--- Practica1/bfs.py
+++ Practica1/bfs.py
@@ -55,10 +55,21 @@ def push(vecino, previos):
     return listaNueva
 
 
-grafo = {
+grafoBase = {
     1:[2, 3],
     2:[1],
     3:[1]
+}
+
+grafo = {
+1: [2, 3],
+2: [1, 4, 5],
+3: [1, 6, 7],
+4: [2],
+5: [2],
+6: [3],
+7: [3, 8],
+8: [7],
 }
 
 bfs(grafo, 1)
--- Practica1/dfs.py
+++ Practica1/dfs.py
@@ -58,10 +58,21 @@ def pop(pila):
     return elemento, nueva_pila
 
 # Grafo de ejemplo representado como lista de adyacencia
-grafo = {
+grafoBase = {
     1: [2, 3],
     2: [1],
     3: [1]
+}
+
+grafo = {
+1: [2, 3],
+2: [1, 4, 5],
+3: [1, 6, 7],
+4: [2],
+5: [2],
+6: [3],
+7: [3, 8],
+8: [7],
 }
 
 # Llamamos a la función DFS con el nodo raíz 1
