27799afd72a3c9d498e5a23fad35fa465464854c
taoreta <nora.castroeduardo@gmail.com>
2024-10-21 09:49:14 -0600

    Punto C de la practica - v1

--- Practica3/matriz.py
+++ Practica3/matriz.py
@@ -15,5 +15,17 @@ array = np.array([
     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 ]).reshape(9, 15)
 
-plt.imshow(array, cmap="gray")
+plt.imshow(array, cmap="gray_r")
+
+#Le preguntamos al usuario una coordenada para pintar el punto rojo
+coordenadaX = int(input("Ingresa la coordenada en X: "))
+coordenadaY = int(input("Ingresa la coordenada en Y: "))
+
+if(coordenadaX>=0 and coordenadaX<=15 and coordenadaY>=0 and coordenadaY <= 9 and array[coordenadaY, coordenadaX] != 1):
+    #Con la siguiente funcion colocamos un punto rojo en las coordenadas dadas
+    plt.scatter(coordenadaX, coordenadaY, color="red", s=100)
+else:
+    print("Coordenada no valida")
+
+
 plt.show()
