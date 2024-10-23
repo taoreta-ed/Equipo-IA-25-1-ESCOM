50fc636df88299af7384baefea612c6c11748bc5
taoreta <nora.castroeduardo@gmail.com>
2024-10-23 16:48:37 -0600

    Correccion en limites de coordenadas

--- Practica3/matriz.py
+++ Practica3/matriz.py
@@ -21,7 +21,7 @@ plt.imshow(array, cmap="gray_r")
 coordenadaX = int(input("Ingresa la coordenada en X: "))
 coordenadaY = int(input("Ingresa la coordenada en Y: "))
 
-if(coordenadaX>=0 and coordenadaX<=15 and coordenadaY>=0 and coordenadaY <= 9 and array[coordenadaY, coordenadaX] != 1):
+if(coordenadaX>=0 and coordenadaX<=14 and coordenadaY>=0 and coordenadaY <= 8 and array[coordenadaY, coordenadaX] != 1):
     #Con la siguiente funcion colocamos un punto rojo en las coordenadas dadas
     plt.scatter(coordenadaX, coordenadaY, color="red", s=100)
 else:
