5bb2b020cef4c6ef4e35d47552dce92533353d04
taoreta <nora.castroeduardo@gmail.com>
2024-11-26 17:09:11 -0600

    Se agrega el archivo para la version bfs de maze runner

--- logica-primer-orden/logicaPrimerOrden.py
+++ logica-primer-orden/logicaPrimerOrden.py
@@ -2,5 +2,11 @@
 pacientes = {
     "Juan": ["tos", "fiebre", "dificultad para respirar"],
     "Ana": ["diarrea", "dolor abdominal", "vomitos"],
-    
+
 }
+
+
+
+for i in range (len(pacientes)):
+    nombre_enfermedad = pacientes
+    #nombre_paciente = 
