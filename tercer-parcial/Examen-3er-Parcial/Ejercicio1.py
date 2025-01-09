c54efd489d05a016e6a5ee7a55280038d35ddf3b
taoreta <nora.castroeduardo@gmail.com>
2025-01-09 17:07:09 -0600

    Add knowledge base and rock-paper-scissors game implementation

--- tercer-parcial/Examen-3er-Parcial/Ejercicio1.py
+++ tercer-parcial/Examen-3er-Parcial/Ejercicio1.py
@@ -1,4 +1,8 @@
-
+#Realizar una base de conocimiento que tengo el nombre del alumno y materias tomadas.
+#3 materias máximo (IA, Machina Learning y Procesamiento de lenguaje)
+#Determinar si el alumno puede tomar una cuarta materia si ha tomado las 3
+#Si ha tomado 2, arrojar la tercera que no está
+#Si sólo tomó 1, arrojar las dos posibles que puede tomar
 relaciones = [
     ["Juan","Pedro"],       #1
     ["Pedro","María"],      #2
