710d98359181bbdd1a6ca8f43a0913f1d134e558
José Ramón Parra Mancilla <164384979+AkiseF@users.noreply.github.com>
2025-01-09 23:11:57 +0000

    Cambios durante examen

--- tercer-parcial/Examen-3er-Parcial/Ejercicio1.py
+++ tercer-parcial/Examen-3er-Parcial/Ejercicio1.py
@@ -1,13 +1,20 @@
-
-relaciones = [
-    ["Juan","Pedro"],       #1
-    ["Pedro","María"],      #2
-    ["Pedro","Luis"],       #3
-    ["Lucía","Pedro"],      #4
-    ["Carlos","Vanessa"],   #5
-    ["Vanessa","Sofía"],    #6
-    ["Vanessa","Diego"],    #7
-    ["Lucía","Matías"],     #8
-    ["Matías","Diego"],     #9
-    ["Elena","Luis"]       #10
+# Ejercicio 1 
+#  Realizar una base de conocimiento que tenga Nombre del alumno y materias tomadas. 3 materias máximo (Int. Artificial, ML y Procesamiento de Lenguaje)
+#  Determinar si el alumno puede tomar una cuerta materia. Si ha tomado las 3 materias, no puede tomar más.
+#  Si ha tomado 2 materias, arrojar la única tercera que no ha tomado.
+#  Si solamente tomó 1 materia arroja las 2 posibles materias que puede tomar. 
+alumnos = [
+    ["Cassandra",["Inteligencia Artificial","Machine Learning","Procesamiento de Lenguaje"]],
+    ["Eduardo",["Procesamiento de Lenguaje","Inteligencia Artificial"]],
+    ["Ramón",["Machine Learning"]]
 ]
+
+materias = ["Inteligencia Artificial","Machine Learning","Procesamiento de Lenguaje"]
+
+for i in range(len(alumnos)):
+
+
+
+
+
+
---
+++ tercer-parcial/Examen-3er-Parcial/Ejercicio3.py
@@ -0,0 +1,87 @@
+#10) Dada la regresión de Ride:
+#    a) Early Stop y que imprima la cantidad de épocas que requirió para hacer el 
+#       elegido < Epsilon, siempre que esté por debajo de las épocas máximas. 
+#    b) Implementar una normalización diferente a las vistas en clase. 
+#    c) Realizarlo para 2 dimensiones (2 características).
+#    d) Implementar la fase de operación.
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Fase de entrenamiento
+#Definición de los datos de entrada
+X_no_norm = np.array([2016,2017,2018,2019,2020,2021,2022,2023,2024])
+#DataSet: Datos ocupados para hacer el entrenamiento
+yd = np.array([4,5,6.5,7,8.5,12,13,16,22])
+#Normalización
+def normalizacion(X_no_norm):
+    x_max = max(X_no_norm)
+    x_min = min(X_no_norm)
+    return (X_no_norm - x_min)/(x_max - x_min)
+
+x_norm = normalizacion(X_no_norm)
+
+X = x_norm
+
+#Definición de hiperparámetros
+lr = 0.01
+epocas = 20000
+b0 = 0.1                        #Se recomienda inicializar entre 0 y 1 de forma random
+b1 = 0.1
+#b = np.array(dimension = m)
+lamda = 0
+m = len(X)                      #Número de muestras
+grafica = []
+
+for epoch in range(epocas):
+    Yobt = b0 + b1*X
+
+    #Calcular el ECM (Error Cuadrático Medio)
+    J = (1/(2*m))*np.sum((yd - Yobt)**2) + (lamda/2)*np.sum(b1**2)
+    grafica += [J]
+    #Calculo de b0 y b
+    b0 = b0 - (lr/m)*np.sum(Yobt - yd)
+    b1 = b1 - (lr/m)*np.sum((Yobt - yd)*X) + (lamda/m)*b1 #lamda*b1 
+
+print(f"Parámetro b0 = {b0}")
+print(f"Parámetro b1 = {b1}")
+
+#Definición de DataTest
+x_test = float(input("Introduce el valor de x: "))
+y_test = b0 + b1*x_test
+print(f"El valor de y es: {y_test}")
+
+#Clasificación 
+x_test_clas = np.array(input("Introduce el valor de x para clasificar: "))
+y_test_clas = np.array(input("Introduce el valor de y para clasificar: "))
+y_obt_clas = b0 + b1*x_test_clas
+if(y_obt_clas > y_test_clas):
+    print("El punto está por encima de la recta")
+else:
+    print("El punto está por debajo de la recta")
+
+
+#Graficar J
+plt.figure(figsize=(8,6))
+plt.plot(range(len(grafica)),grafica,label="Función de costo")
+plt.xlabel("Épocas")
+plt.ylabel("J")
+plt.title("Comportamiento de J")
+plt.legend()
+plt.show()
+
+plt.figure(figsize=(8,6))
+for i in range(m):
+    plt.scatter(X[i],yd[i])
+
+x_val = [min(X),max(X)]
+y_val = [(b0 + b1*x) for x in x_val]
+plt.plot(x_val,y_val)
+plt.show()
+
+#Fase de operación: Para hacer estimaciones
+X_test = float(input("Introduce el año a predecir: "))
+X_normalizada = normalizacion(np.array([X_test]))
+Yobt_test = b0 + b1*X_normalizada
+print(f"Para el año {X_test} se estiman {Yobt_test} millones de ventas de coches")
+
