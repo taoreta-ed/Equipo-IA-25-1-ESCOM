6ce820aed454bd72e775e0a235a3655c954cea03
José Ramón Parra Mancilla <164384979+AkiseF@users.noreply.github.com>
2025-01-09 16:46:47 -0600

    Add files via upload

---
+++ tercer-parcial/Examen-3er-Parcial/Ejercicio1.py
@@ -0,0 +1,13 @@
+
+relaciones = [
+    ["Juan","Pedro"],       #1
+    ["Pedro","María"],      #2
+    ["Pedro","Luis"],       #3
+    ["Lucía","Pedro"],      #4
+    ["Carlos","Vanessa"],   #5
+    ["Vanessa","Sofía"],    #6
+    ["Vanessa","Diego"],    #7
+    ["Lucía","Matías"],     #8
+    ["Matías","Diego"],     #9
+    ["Elena","Luis"]       #10
+]
