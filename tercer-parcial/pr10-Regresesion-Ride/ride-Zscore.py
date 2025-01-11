dca260ce2d599fd56b484bee9ad02156a04fbfca
taoreta <nora.castroeduardo@gmail.com>
2025-01-11 15:49:41 -0600

    Add Z-score normalization and linear regression implementation for car sales prediction

---
+++ tercer-parcial/pr10-Regresesion-Ride/ride-Zscore.py
@@ -0,0 +1,70 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Fase de entrenamiento
+#Definición de los datos de entrada
+X_no_norm = np.array([[2019,4], [2020,2], [2021,4], [2022,4], [2023,2], [2024,4]]) #Año y segunda caracteristica (número de puertas)
+#Etiquetas de salida
+yd = np.array([4, 5, 6.5, 7, 8.5, 12])
+
+#Normalización usando Z-score
+def normalizacion_zscore(X):
+    media = np.mean(X, axis=0)
+    desviacion = np.std(X, axis=0)
+    return (X - media)/desviacion, media, desviacion
+
+def main():
+    #Normalizar las caracteristicas
+    X, media, desviacion = normalizacion_zscore(X_no_norm)
+
+    #Definición de hiperparámetros
+    lr = 0.01 #Tasa de aprendizaje
+    max_epocas = 20000 #Número de iteraciones
+    epsilon = 1e-6 #Criterio de paro
+    b = np.random.rand(3) #Inicialización de los parámetros (b0, b1 y b2)
+    lamda = 0 #Factor de regularización
+    m = len(X) #Número de muestras
+    grafica = []
+
+    #Entrenamiento con Early Stop
+    early_stop_epoch = max_epocas
+    for epoch in range(max_epocas):
+        #Calcular Y obtenido
+        Yobt = b[0] + b[1]*X[:,0] + b[2]*X[:,1]
+
+        #Calcular el Error Cuadrático Medio
+        J = (1 / (2*m)) * np.sum((yd - Yobt)**2) + (lamda / (2*m)) * np.sum(b[1:]**2)
+        grafica += [J]
+
+        #Verificar Early Stop
+        if J < epsilon:
+            early_stop_epoch = epoch + 1
+            break
+
+        #Gradiente descendente
+        b[0] -= (lr/m) * np.sum(Yobt - yd)
+        b[1] -= (lr/m) * np.sum((Yobt - yd) * X[:, 0]) + (lamda/m) * b[1]
+        b[2] -= (lr/m) * np.sum((Yobt - yd) * X[:, 1]) + (lamda/m) * b[2]
+
+    #Resultados del entrenamiento
+    print(f"Parámetros finales b0 = {b[0]}, b1 = {b[1]}, b2 = {b[2]}")
+    print(f"Épocas ejecutadas: {early_stop_epoch}")
+
+    #Graficar la función de costo
+    plt.figure(figsize=(8,6))
+    plt.plot(range(len(grafica)), grafica, label="Función de costo")
+    plt.xlabel("Épocas")
+    plt.ylabel("J (ECM)")
+    plt.title("Comportamiento de J")
+    plt.legend()
+    plt.show()
+
+    #Fase de operación: Para hacer estimaciones
+    X_test = np.array([[2025, 2]]) #Año y segunda caracteristica
+    X_test_norm = (X_test - media)/desviacion #Normalizar el dato de prueba
+    X_test_norm = X_test_norm.reshape(1, -1) #Ajustar la forma del dato de prueba
+
+    Y_test = b[0] + b[1]*X_test_norm[0,0] + b[2]*X_test_norm[0,1]
+    print(f"Para el año {X_test[0,0]} con característica adicional {X_test[0,1]} se estiman {Y_test:.2f} millones de ventas de coches")
+
+main()
